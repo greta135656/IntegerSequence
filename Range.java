e6c586f4ef5ab8a20eeb1afa858e226dc90e74fe
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-05 19:34:54 +0000

    everything should be good

--- Range.java
+++ Range.java
@@ -1,20 +1,22 @@
-
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
+
+
 
   //@param start : the starting value (inclusive) which must be <= end.
   //@param end : the ending value which is also inclusive.
   public Range(int start,  int end){ 
   this.start = start;
   this.end = end;
+  current = start;
    }
-
+ 
   public void reset(){  
   current = start;}
   
   public int length(){ 
-  return end - start; }
+  return Math.abs(end - start); }
   
   //When current is no longer a valid element in the range, it should return false. 
   public boolean hasNext(){ 
@@ -28,6 +30,8 @@ public class Range implements IntegerSequence{
   public int next() {   
   if (!hasNext())
    {throw new  NoSuchElementException("error");}
-   {return current+1;}}
+   {current++;
+return current-1;}}
+
   
 }
