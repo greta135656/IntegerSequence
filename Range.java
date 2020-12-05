1a230a69c656cda560bb3aebb6f5d5e511252dfe
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-05 19:09:44 +0000

    added next

--- Range.java
+++ Range.java
@@ -25,6 +25,9 @@ public class Range implements IntegerSequence{
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by 1. 
   //e.g.  if current is 5. This will make current 6, and return 5. 
-  public int next(){    }
+  public int next() {   
+  if (!hasNext())
+   {throw new  NoSuchElementException("error");}
+   {return current+1;}}
   
 }
