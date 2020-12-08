b9ae5154b10c9deed842c55dee9dccd8203a585f
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-08 17:50:46 +0000

    added next, length, hasNext, reset

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,26 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+  data = new int[other.length];
+  for (int i = 0; i < data.length; i ++){
+  data [i] = other[i];
+  } }
+  
+public void reset(){
+currentIndex = 0;
 }
+
+public int length(){ 
+return Math.abs(data.length);}
+
+public boolean hasNext(){ 
+  if (currentIndex < data.length)
+  return true;
+  return false; }
+
+  public int next() {   
+  if (!hasNext())
+   {throw new  NoSuchElementException("error");}
+   {currentIndex++;
+return currentIndex-1;}}}
