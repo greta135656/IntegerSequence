5cc2cc46987a732c9160df672d30da329ea3b153
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-09 17:06:40 +0000

    fixed constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -16,8 +16,10 @@ public ArraySequence(IntegerSequence otherseq){
 otherseq.reset();
 data = new int[otherseq.length()];
 for (int i = 0; i < otherseq.length(); i++){
+if (otherseq.hasNext())
 data[i] = otherseq.next();
 }
+currentIndex=0;
 }
 
   
